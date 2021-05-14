1e4a905d058311cc4f9c0cea5a8725afffe1229b
Aditya <defuse805@gmail.com>
2021-05-14 07:14:46 +0700

    another step

---
+++ home/staticfiles/vendor/js/script.js
@@ -0,0 +1,9 @@
+const bungkus=document.querySelector('.bungkus');
+const body=document.body
+
+window.addEventListener('load',function(){
+    bungkus.className+=' hidden';
+})
+
+const text=document.querySelector('.myportfolio-text p').innerHTML
+console.log(text)
